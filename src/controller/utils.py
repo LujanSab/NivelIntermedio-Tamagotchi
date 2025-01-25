3cc78c9865977dc4a0991224123cc5aee458a5a6
Emilio Luna <lunaemilio2003@gmail.com>
2025-01-25 19:42:25 -0300

    #3 traduccion comentarios y eliminacion de importacion inutil

--- src/controller/utils.py
+++ src/controller/utils.py
@@ -1,19 +1,22 @@
-from src.config import Styles
 import pygame
 
 
 def scale_img(image, scale = 1.0):
     """
-    The function `scale_img` takes an image and scales it by a specified factor using pygame's
-    `transform.scale` method.
+    La función `scale_img` toma una imagen y la escala según un factor específico utilizando el método
+    `transform.scale` de Pygame.
+
+    parametros:
+    ----------
+        - image: El parámetro `image` es la imagen que desea escalar. Normalmente es un objeto de superficie
+        que representa una imagen en Pygame
+        - scale: El parámetro `scale` en la función `scale_img` se utiliza para determinar el factor
+        por el cual se debe escalar la imagen. De manera predeterminada, está configurado en `Styles.DOG_IMAGE`, lo que sugiere que
+        la imagen se escalará según un valor de escala predefinido para una imagen de perro. Puede
     
-    :param image: The `image` parameter is the image that you want to scale. It is typically a surface
-    object representing an image in Pygame
-    :param scale: The `scale` parameter in the `scale_img` function is used to determine the factor by
-    which the image should be scaled. By default, it is set to `Styles.DOG_IMAGE`, which suggests that
-    the image will be scaled based on a predefined scale value for a dog image. You can
-    :return: The function `scale_img` returns a new image that has been scaled based on the specified
-    scale factor.
+    return:
+    -------
+        - devuelve una nueva imagen que se ha escalado según el factor de escala especificado.
     """
     width = int(image.get_width() * scale)
     height = int(image.get_height() * scale)
