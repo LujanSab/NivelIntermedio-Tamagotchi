84d066b7f90b6a857b824d5e395b3853e81b6e29
LujanSab <luyumm16@gmail.com>
2025-01-25 14:58:37 -0300

    Agregué clases Perro y Hamster

--- src/models/mascotas/mascotas.py
+++ src/models/mascotas/mascotas.py
@@ -1,2 +1,26 @@
 class Mascota:
-    pass
+    def __init__(self, nombre_dueño, nombre_mascota):
+        self.dueño = nombre_dueño
+        self.nombre = nombre_mascota
+        self.limpieza = 50
+        self.hambre = 50
+        self.felicidad = 50
+
+    def limpiarse(self):
+        pass
+
+    def comer(self):
+        pass
+
+    def jugar(self):
+        pass
+
+class Perro(Mascota):
+    def __init__(self, nombre_dueño, nombre_perro):
+        super().__init__(nombre_dueño, nombre_perro)
+        self.tipo = "Perro"
+
+class Hamster(Mascota):
+    def __init__(self, nombre_dueño, nombre_hamster):
+        super().__init__(nombre_dueño, nombre_hamster)
+        self.tipo = "Hamster"
