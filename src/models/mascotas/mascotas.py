b29573e4977b52873d6fda5d48304c5f36b512d7
Emilio Luna <lunaemilio2003@gmail.com>
2025-01-25 19:51:33 -0300

    cambios en los modelos

--- src/models/mascotas/mascotas.py
+++ src/models/mascotas/mascotas.py
@@ -1,7 +1,5 @@
-from abc import ABC, abstractmethod
 
-class Mascota(ABC):
-    @abstractmethod
+class Mascota:
     def __init__(self, nombre_dueño, nombre_mascota, tipo):
         self.dueño = nombre_dueño
         self.nombre = nombre_mascota
@@ -12,72 +10,58 @@ class Mascota(ABC):
         self.felicidad = 50
 
     @property
-    @abstractmethod
     def get_nombre_dueño(self):
         return self.dueño
     
     @get_nombre_dueño.setter
-    @abstractmethod
     def set_nombre_dueño(self, nombre_dueño):
         self.dueño = nombre_dueño
 
     @property
-    @abstractmethod
     def get_nombre_mascota(self):
         return self.nombre
     
     @get_nombre_mascota.setter
-    @abstractmethod
     def set_nombre_mascota(self, nombre_mascota):
         self.nombre = nombre_mascota
     
     @property
-    @abstractmethod
     def get_tipo_de_mascota(self):
         return self.tipo
     
     @get_tipo_de_mascota.setter
-    @abstractmethod
     def set_tipo_de_mascota(self, tipo):
         self.tipo = tipo
     
     @property
-    @abstractmethod
     def get_energia(self):
         return self.energia
     
     @get_energia.setter
-    @abstractmethod
     def set_energia(self, valor):
         self.energia += valor
 
     @property
-    @abstractmethod
     def get_limpieza(self):
         return self.limpieza
 
     @get_limpieza.setter
-    @abstractmethod
     def set_limpieza(self, valor):
         self.limpieza += valor
 
     @property
-    @abstractmethod
     def get_hambre(self):
         return self.hambre
     
     @get_hambre.setter
-    @abstractmethod
     def set_hambre(self, valor):
         self.hambre += valor
 
     @property
-    @abstractmethod
     def get_felicidad(self):
         return self.felicidad
 
     @get_felicidad.setter
-    @abstractmethod
     def set_felicidad(self, valor):
         self.felicidad += valor
 
