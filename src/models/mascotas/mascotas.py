271766586a7607390174cd68600d15e157cd118b
LujanSab <luyumm16@gmail.com>
2025-01-25 15:42:15 -0300

    Agregue setter y getters en Mascota

--- src/models/mascotas/mascotas.py
+++ src/models/mascotas/mascotas.py
@@ -1,55 +1,85 @@
 from abc import ABC, abstractmethod
 
 class Mascota(ABC):
+    @abstractmethod
     def __init__(self, nombre_dueño, nombre_mascota, tipo):
         self.dueño = nombre_dueño
         self.nombre = nombre_mascota
         self.tipo = tipo
+        self.energia = 50
         self.limpieza = 50
         self.hambre = 50
         self.felicidad = 50
 
     @property
     @abstractmethod
-    def nombre_dueño(self):
+    def get_nombre_dueño(self):
         return self.dueño
     
-    @nombre_dueño.setter
+    @get_nombre_dueño.setter
     @abstractmethod
     def set_nombre_dueño(self, nombre_dueño):
         self.dueño = nombre_dueño
 
     @property
     @abstractmethod
-    def nombre_mascota(self):
+    def get_nombre_mascota(self):
         return self.nombre
     
-    @nombre_mascota.setter
+    @get_nombre_mascota.setter
     @abstractmethod
     def set_nombre_mascota(self, nombre_mascota):
         self.nombre = nombre_mascota
     
     @property
     @abstractmethod
-    def tipo_de_mascota(self):
+    def get_tipo_de_mascota(self):
         return self.tipo
     
-    @tipo_de_mascota.setter
+    @get_tipo_de_mascota.setter
     @abstractmethod
     def set_tipo_de_mascota(self, tipo):
         self.tipo = tipo
     
+    @property
     @abstractmethod
-    def limpiarse(self):
-        pass
+    def get_energia(self):
+        return self.energia
+    
+    @get_energia.setter
+    @abstractmethod
+    def set_energia(self, valor):
+        self.energia += valor
 
+    @property
     @abstractmethod
-    def comer(self):
-        pass
+    def get_limpieza(self):
+        return self.limpieza
 
+    @get_limpieza.setter
     @abstractmethod
-    def jugar(self):
-        pass
+    def set_limpieza(self, valor):
+        self.limpieza += valor
+
+    @property
+    @abstractmethod
+    def get_hambre(self):
+        return self.hambre
+    
+    @get_hambre.setter
+    @abstractmethod
+    def set_hambre(self, valor):
+        self.hambre += valor
+
+    @property
+    @abstractmethod
+    def get_felicidad(self):
+        return self.felicidad
+
+    @get_felicidad.setter
+    @abstractmethod
+    def set_felicidad(self, valor):
+        self.felicidad += valor
 
 class Perro(Mascota):
     def __init__(self, nombre_dueño, nombre_perro, tipo):
