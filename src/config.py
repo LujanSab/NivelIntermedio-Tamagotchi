2701eee2b93ad52c09de2db793693ee9bf931fec
Emilio Luna <lunaemilio2003@gmail.com>
2025-01-25 20:30:22 -0300

    cambios en las configuraciones

--- src/config.py
+++ src/config.py
@@ -1,6 +1,15 @@
 from dataclasses import dataclass
+import pygame
+from src.controller.utils import scale_img
 
 @dataclass
 class Styles:
-    MASCOTA_WIDTH = 500
-    MASCOTA_HEIGHT = 800
+
+    WINDOW_HEIGHT = 800
+    WINDOW_WIDTH = 1500
+
+    MASCOTA_SCALE = 4.0
+    dog_img = pygame.image.load('assets//dog.png')
+    DOG_IMAGE = scale_img(dog_img, MASCOTA_SCALE)
+    MASCOTA_WIDTH = DOG_IMAGE.get_width()
+    MASCOTA_HEIGHT = DOG_IMAGE.get_height()
