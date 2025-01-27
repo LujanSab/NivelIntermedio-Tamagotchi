5b8ba5679b1cabb77046b83882e759a65965292e
Emilio Luna <lunaemilio2003@gmail.com>
2025-01-27 16:13:25 -0300

    #4 correccion error de referencia de sprite

--- src/config.py
+++ src/config.py
@@ -9,7 +9,7 @@ class Styles:
     WINDOW_WIDTH = 1500
 
     MASCOTA_SCALE = 4.0
-    dog_img = pygame.image.load('assets//dog.png')
+    dog_img = pygame.image.load('assets//mascotas//perro//PERRO1.png')
     DOG_IMAGE = scale_img(dog_img, MASCOTA_SCALE)
     MASCOTA_WIDTH = DOG_IMAGE.get_width()
     MASCOTA_HEIGHT = DOG_IMAGE.get_height()
