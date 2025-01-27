10569ae3638f44aef6499a2f84ea1132fec770d0
Emilio Luna <lunaemilio2003@gmail.com>
2025-01-27 15:16:44 -0300

    #4 nuevos botones interactivos en la vista, nueva funcion log, cambios en la entidad mascota y cambios en las configuraciones

---
+++ src/controller/logger.py
@@ -0,0 +1,19 @@
+import logging
+import os
+from src.config import BASE_DIR
+
+log_directory = os.path.join(BASE_DIR, 'logs')
+
+os.makedirs(log_directory, exist_ok=True)
+
+log_file_path = os.path.join(log_directory, 'logs.log')
+
+logging.basicConfig(
+    filename=log_file_path,
+    filemode='a',
+    level=logging.INFO,
+    format="%(asctime)s - %(message)s",
+)
+
+def log(event):
+    logging.info(event)
