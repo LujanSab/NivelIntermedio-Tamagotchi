95333e44fdbbbbb1e39843f8d1c27afb1fef94e0
Emilio Luna <eluna@psa.local>
2025-01-28 15:41:42 -0300

    #6 cambios en servicio y adaptacion en dao

--- src/models/mascotas/mascotaService.py
+++ src/models/mascotas/mascotaService.py
@@ -1,4 +1,5 @@
 from src.models.mascotas.mascotaDAO import MascotaDAO
+from src.controller.logger import log
 
 
 class MascotaService:
@@ -20,6 +21,36 @@ class MascotaService:
         mascota = self.dao.extraer_datos_mascota(nombre)
 
         return mascota
+    
+    def actualizar(self, nombre, energia=None, limpieza=None, hambre=None, felicidad=None):
+        campos = []
+        valores = []
+
+        if energia is not None:
+            campos.append("energia = ?")
+            valores.append(energia)
+        
+        if limpieza is not None:
+            campos.append("limpieza = ?")
+            valores.append(limpieza)
+        
+        if hambre is not None:
+            campos.append("hambre = ?")
+            valores.append(hambre)
+
+        if felicidad is not None:
+            campos.append("felicidad = ?")
+            valores.append(felicidad)
+
+        if not campos:
+            log("No se proporcionaron valores para actualizar.")
+            print("No se proporcionaron valores para actualizar.")
+
+        campos_str = ', '.join(campos)
+        valores.append(nombre)
+
+        self.dao.actualizar_estado_mascota(campos=campos_str, valores=valores)
+
     
     def __str__(self):
         return str(self.mascota.nombre_mascota)
