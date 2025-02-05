62b68d3ffab4c52640ed316cd1bae698244f9ecf
Emilio Luna <lunaemilio2003@gmail.com>
2025-02-05 13:16:58 -0300

    #4 cambio de lugar de cierta logica y constantes

--- config.py
+++ config.py
@@ -13,27 +13,49 @@ FPS = 60
 
 @dataclass
 class Config:
-
+    '''
+    Dataclass Config construida para almacenar constantes sobre el juego
+    '''
     WINDOW_HEIGHT = 700
     WINDOW_WIDTH = 1300
 
     MASCOTA_SCALE = 4.0
-    dog_img = pygame.image.load(f'{ASSETS_UTL}//mascotas//perro//PERRO1.png')
-    DOG_IMAGE = scale_img(dog_img, MASCOTA_SCALE)
+
+    _dog_img = pygame.image.load(f'{ASSETS_UTL}//mascotas//perro//PERRO1.png')
+    DOG_IMAGE = scale_img(_dog_img, MASCOTA_SCALE)
+
+    _cat_img = pygame.image.load(f'{ASSETS_UTL}//mascotas//gato//GATO1.png')
+    CAT_IMG = scale_img(_cat_img, MASCOTA_SCALE)
+
     MASCOTA_WIDTH = DOG_IMAGE.get_width()
     MASCOTA_HEIGHT = DOG_IMAGE.get_height()
 
-    dormir_emote = pygame.image.load(f'{ASSETS_UTL}//emotes//durmiendo//DORMIR1.png')
-    DORMIR_IMG = scale_img(dormir_emote, 1.8)
+    DORMIR_EMOTE = []
+    FELIZ_EMOTE = []
+    HAMBRE_EMOTE = []
+    LIMPIAR_EMOTE = []
 
-    feliz_emote = pygame.image.load(f'{ASSETS_UTL}//emotes//felicidad//FELICIDAD1.png')
-    FELIZ_IMG = scale_img(feliz_emote, 1.8)
+    GATO_IDLE = []
 
-    hambre_emote = pygame.image.load(f'{ASSETS_UTL}//emotes//hambre//HAMBRE1.png')
-    HAMBRE_IMG = scale_img(hambre_emote, 1.8)
+    for i in range(10):
+            new_img = scale_img(pygame.image.load(f'{ASSETS_UTL}//mascotas//gato//GATO{i+1}.png'), MASCOTA_SCALE)
+            GATO_IDLE.append(new_img)
 
-    limpiar_emote = pygame.image.load(f'{ASSETS_UTL}//emotes//limpiar//LIMPIAR1.png')
-    LIMPIAR_IMG = scale_img(limpiar_emote, 1.8)
+    for i in range(2):
+            new_img = scale_img(pygame.image.load(f'{ASSETS_UTL}//emotes//durmiendo//DORMIR{i+1}.png'), 1.8)
+            DORMIR_EMOTE.append(new_img)
+
+    for i in range(3):
+            new_img = scale_img(pygame.image.load(f'{ASSETS_UTL}//emotes//felicidad//FELICIDAD{i+1}.png'), 1.8)
+            FELIZ_EMOTE.append(new_img)
+
+    for i in range(2):
+            new_img = scale_img(pygame.image.load(f'{ASSETS_UTL}//emotes//hambre//HAMBRE{i+1}.png'), 1.8)
+            HAMBRE_EMOTE.append(new_img)
+
+    for i in range(2):
+            new_img = scale_img(pygame.image.load(f'{ASSETS_UTL}//emotes//limpiar//LIMPIAR{i+1}.png'), 1.8)
+            LIMPIAR_EMOTE.append(new_img)
 
     BTN_WIDTH = 150
     BTN_HEIGHT = 50
