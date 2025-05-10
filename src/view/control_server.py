c87d6c048895673671436eff08048fb6674f3e59
LujanSab <luyumm16@gmail.com>
2025-05-10 13:15:59 -0300

    #issue-90 Socket

--- src/view/control_server.py
+++ src/view/control_server.py
@@ -4,29 +4,33 @@ from pathlib import Path
 import subprocess
 import threading
 
-theproc=None
+theproc=""
 
 class ControlServer:
     def __init__(self):
         self.raiz = Path(__file__).resolve().parent.parent
         self.ruta_server = os.path.join(self.raiz, 'controller', 'server.py')
         
-    def try_connection(self): 
-        global theproc
-        if theproc is not None and theproc.poll() is None:  # El proceso ya está corriendo
-            print("El servidor ya está corriendo.")
+    def try_connection(self, ): 
+        if theproc != "":
             theproc.kill()
-            threading.Thread(target=self.lanzar_servidor(), args=(True,), daemon=True).start()
+            threading.Thread(target=self.lanzar_servidor, args=(True,), daemon=True).start()
         else:
-            threading.Thread(target=self.lanzar_servidor(), args=(True,), daemon=True).start()
+            threading.Thread(target=self.lanzar_servidor, args=(True,), daemon=True).start()
         
-    def lanzar_servidor(self):
-        global theproc
-        the_path = self.ruta_server
-        theproc = subprocess.Popen([sys.executable, the_path])
+    def lanzar_servidor(self, var):
+        the_path =  self.ruta_server
+        if var==True:
+            global theproc
+            theproc = subprocess.Popen([sys.executable, the_path])
+            theproc.communicate()
+        else:
+            print("")
 
-    def stop_server(self):
+    # =================== INNIT AND STOP SERVER ====================== 
+    def stop_server(self, ):
         global theproc
-        if theproc is not None and theproc.poll() is None:
-            theproc.kill()
-            theproc = None
+        if theproc !="":
+            theproc.kill() 
+
+ 
